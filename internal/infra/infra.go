3b6cffe20e771fd7f695aa69858c41e3b7e56a1b
Aleksey Martunov <alekseymartunov@icloud.com>
2024-05-11 13:18:37 +0300

    create logger

--- internal/infra/infra.go
+++ internal/infra/infra.go
@@ -1,7 +1,17 @@
 package infra
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"tradingbot/pkg/tcplogger"
+)
 
 func RunApp(ctx context.Context) error {
+	logger, err := tcplogger.NewLogger("trace", "127.0.0.1:5170", true)
+	if err != nil {
+		return fmt.Errorf("creation logger error: %w", err)
+	}
+	defer logger.Close()
+
 	return nil
 }
---
+++ pkg/tcplogger/logger.go
@@ -0,0 +1,115 @@
+package tcplogger
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+type writerHook struct {
+	Writer    io.Writer
+	LogLevels []logrus.Level
+	Formatter logrus.Formatter
+}
+
+func (h *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := h.Formatter.Format(entry)
+	if err != nil {
+		return err
+	}
+	_, err = h.Writer.Write(line)
+	return err
+}
+
+func (h *writerHook) Levels() []logrus.Level {
+	return h.LogLevels
+}
+
+type Logger struct {
+	conn net.Conn
+}
+
+func NewLogger(level string, logServiceAddr string, setReportCaller bool) (*Logger, error) {
+	lvl, err := logrus.ParseLevel(level)
+	if err != nil {
+		lvl = logrus.TraceLevel
+	}
+
+	logrus.SetReportCaller(setReportCaller)
+	logrus.SetLevel(lvl)
+	logrus.SetOutput(io.Discard)
+
+	conn, err := net.Dial("tcp", logServiceAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	logstashHook := writerHook{
+		Writer:    conn,
+		LogLevels: logrus.AllLevels,
+		Formatter: &logrus.JSONFormatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+			CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+				return "", fmt.Sprintf("%s:%d", frame.File, frame.Line)
+			},
+		},
+	}
+
+	stdoutHook := writerHook{
+		Writer:    os.Stdout,
+		LogLevels: logrus.AllLevels,
+		Formatter: &logrus.TextFormatter{
+			PadLevelText:           true,
+			ForceColors:            true,
+			DisableLevelTruncation: true,
+			TimestampFormat:        "2006-01-02 15:04:05",
+			FullTimestamp:          true,
+			CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
+				return "", fmt.Sprintf("%s:%d", frame.File, frame.Line)
+			},
+		},
+	}
+
+	logrus.AddHook(&stdoutHook)
+	logrus.AddHook(&logstashHook)
+
+	return &Logger{
+		conn: conn,
+	}, nil
+}
+
+func (l *Logger) Close() error {
+	return l.conn.Close()
+}
+
+func (l *Logger) Trace(message string) {
+	logrus.Trace(message)
+}
+
+func (l *Logger) Debug(message string) {
+	logrus.Debug(message)
+}
+
+func (l *Logger) Info(message string) {
+	logrus.Info(message)
+}
+
+func (l *Logger) Warn(message string) {
+	logrus.Warn(message)
+}
+
+func (l *Logger) Error(message string) {
+	logrus.Error(message)
+}
+
+func (l *Logger) Fatal(message string) {
+	logrus.Fatal(message)
+}
+
+func (l *Logger) Panic(message string) {
+	logrus.Panic(message)
+}
